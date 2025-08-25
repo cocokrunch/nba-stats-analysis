b4a1264cce4bd9ff7b02df0873fb0972320af729
Kim Dionisio <136983393+cocokrunch@users.noreply.github.com>
2025-08-25 21:13:10 +0800

    Update top_scorers_2022.sql

--- queries/top_scorers_2022.sql
+++ queries/top_scorers_2022.sql
@@ -7,8 +7,8 @@ FROM player_stats AS ps
 JOIN players AS pl ON ps.player_id = pl.player_id
 JOIN teams AS tm ON ps.team_id = tm.team_id
 JOIN seasons AS s ON ps.season_id = s.season_id
-WHERE s.season = 2022
+WHERE s.season BETWEEN 2010 AND 2022
 GROUP BY pl.player_name, tm.team_abbreviation, s.season
 HAVING total_points > 0
 ORDER BY total_points DESC
-LIMIT 10;
+LIMIT 10; -- Limit by Top 10 only
